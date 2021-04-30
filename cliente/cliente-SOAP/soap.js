85ee786955addeb34553bca1efda7c41df6176d4
Luzmarisol1 <zs17015187@estudiantes.uv.mx>
2021-04-30 11:29:05 -0500

    Actuaizaciones Eliminar

--- cliente/cliente-SOAP/soap.js
+++ cliente/cliente-SOAP/soap.js
@@ -1,4 +1,5 @@
 var ventas;
+var elimp;
 
 function agregar() {
     ventas = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
@@ -21,3 +22,43 @@ function venta() {
         },
     })
 }
+
+function eliminar() {
+    elimp = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
+        '<Body>' +
+        '<EliminarVentaRequest xmlns="http://tell.me/ventas">' +
+        '<id>' + document.getElementById('id').value + '</id>' +
+        '</EliminarVentaRequest>' +
+        '</Body>' +
+        '</Envelope>'
+}
+
+function idElim() {
+    eliminar()
+    axios.post('http://localhost:8080/ws/venta', elimp, {
+        headers: {
+            'Content-Type': 'text/xml',
+            'SOAPAction': ''
+        },
+    })
+}
+
+
+/*function visualizar() {
+    verVentas = '<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">' +
+        '<Body>' +
+        '<MostrarVentaRequest xmlns="http://tell.me/ventas">' +
+
+        '</MostrarVentaRequest>'
+        '</Envelope>'
+}
+function visualizarV(){
+    visualizar()
+    axios.post('http://localhost:8080/ws/venta', ventas, {
+        headers: {
+            'Content-Type': 'text/xml',
+            'SOAPAction': ''
+        },
+    })
+
+}*/
